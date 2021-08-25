f6ae184648db8fa0a3f6053cf4a67a3c813891dd
Diogo Mendes <contato.diogomendes@gmail.com>
2021-08-24 21:44:37 -0300

    Create json for landing texts and create button component

--- src/web/pages/Home/index.tsx
+++ src/web/pages/Home/index.tsx
@@ -2,8 +2,9 @@ import { useTranslation } from "next-i18next";
 
 import { FCWithLayout } from "types/interfaces/layout";
 
+// eslint-disable-next-line jest/no-export
 export const HomePage: FCWithLayout = () => {
-	const { t } = useTranslation("home");
+	const { t } = useTranslation("landing");
 
-	return <>{t("message")}</>;
+	return <>{t("header.title")}</>;
 };
